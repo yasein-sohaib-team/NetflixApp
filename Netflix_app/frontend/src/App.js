d649896ce68ce6b0f0a1ff74ac276e95f072602f
Yasein Burqan <mr.yasein@gmail.com>
2022-05-18 17:44:08 +0300

    NetflixApp

--- Netflix_app/frontend/src/App.js
+++ Netflix_app/frontend/src/App.js
@@ -5,9 +5,18 @@ import requests from "./Requests";
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals} />
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+
+
+
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
